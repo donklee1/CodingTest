020944d8289feff23959027cf714d772f34df714
동기 이 <ldk@true-tech.co.kr>
2024-04-08 23:52:09 +0900

    V0408H

--- Coding.py
+++ Coding.py
@@ -1,13 +1,27 @@
-# 17219 S4
+# 2579 S3
 from sys import stdin
 
-site_count, qurey_count = map(int, stdin.readline().split())
-SITE = {}
+count = int(stdin.readline())
+STEP = []
+for i in range(count):
+    val = int(stdin.readline().rstrip())
+    STEP.append(val)
 
-for _ in range(site_count):
-    site, password = stdin.readline().split()
-    SITE[site] = password
+DP_MAXVAL = [0] * count
+ONE_COUNT = [0] * count
 
-for _ in range(qurey_count):
-    site = stdin.readline().rstrip()
-    print(SITE[site])
+DP_MAXVAL[0] = STEP[0]
+ONE_COUNT[0] = 1
+
+for i in range(1, count):
+    max_val = 0
+    if (ONE_COUNT[i-1] < 2):
+        max_val = DP_MAXVAL[i-1] + STEP[i]
+        ONE_COUNT[i] = ONE_COUNT[i-1] + 1
+    if (i >= 2) and ((DP_MAXVAL[i-2]) + STEP[i] > max_val):
+        max_val = DP_MAXVAL[i-2] + STEP[i]
+        ONE_COUNT[i] = 0
+    DP_MAXVAL[i] = max_val
+    print("i=", i, "ONE=", ONE_COUNT, "DP=", DP_MAXVAL)
+    
+print(DP_MAXVAL[count-1])
