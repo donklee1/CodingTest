6555776700c6e6969f242c67cd9598c158cecee0
donklee1 <ldk@true-tech.co.kr>
2024-04-08 20:51:51 +0900

    V0408

--- Coding.py
+++ Coding.py
@@ -1,32 +1,13 @@
-# 11723 s5
+# 17219 S4
 from sys import stdin
 
-DATA = set()
-M = int(stdin.readline().rstrip())
-for _ in range(M):
-    cmd = list(stdin.readline().split())
-    if cmd[0] == "add":
-        param = int(cmd[1])
-        DATA.add(param)
-    elif cmd[0] == "remove":
-        param = int(cmd[1])
-        if (param in DATA) == True:
-            DATA.remove(param)
-    elif cmd[0] == "check":
-        param = int(cmd[1])
-        if (param in DATA) == True:
-            print(1)
-        else:
-            print(0)
-    elif cmd[0] == "toggle":
-        param = int(cmd[1])
-        if (param in DATA) == True:
-            DATA.remove(param)
-        else:
-            DATA.add(param)
-    elif cmd[0] == "all":
-        DATA.clear()
-        for i in range(1, 21, 1):
-             DATA.add(i)
-    elif cmd[0] == "empty":
-        DATA.clear()
+site_count, qurey_count = map(int, stdin.readline().split())
+SITE = {}
+
+for _ in range(site_count):
+    site, password = stdin.readline().split()
+    SITE[site] = password
+
+for _ in range(qurey_count):
+    site = stdin.readline().rstrip()
+    print(SITE[site])
