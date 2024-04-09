752ef51975c32b685ac8f6eefd36ca60ae42b661
donklee1 <ldk@true-tech.co.kr>
2024-04-09 18:28:54 +0900

    V0409

--- Coding.py
+++ Coding.py
@@ -1,27 +1,36 @@
-# 2579 S3
+# 21314 S1
 from sys import stdin
 
-count = int(stdin.readline())
-STEP = []
-for i in range(count):
-    val = int(stdin.readline().rstrip())
-    STEP.append(val)
+S = stdin.readline().strip()
 
-DP_MAXVAL = [0] * count
-ONE_COUNT = [0] * count
+# --- Token 분리 ---
+token = []
+sub_str = ""
+for s1 in S:
+    if s1 != "K":
+        sub_str += s1
+    else:
+        sub_str += "K"
+        token.append(sub_str)
+        sub_str = ""
+if (sub_str != ""):
+    token.append(sub_str)
 
-DP_MAXVAL[0] = STEP[0]
-ONE_COUNT[0] = 1
+min_str = ""
+max_str = ""
+for tok in token:
+    m_count = str(tok).count("M")
+    print("tok=", tok)
+    if "K" in tok: # MMMMMK 타입
+        if m_count == 0:
+            max_str += "5"
+            min_str += "5"
+        else:
+            max_str += str(5 * (10 ** m_count)) # 500..
+            min_str += str((10 ** m_count) + 5) # 100.. 5
+    else: # MMMMM 타입
+        max_str += str(('1' * m_count))
+        min_str += str(10 ** (m_count-1))
 
-for i in range(1, count):
-    max_val = 0
-    if (ONE_COUNT[i-1] < 2):
-        max_val = DP_MAXVAL[i-1] + STEP[i]
-        ONE_COUNT[i] = ONE_COUNT[i-1] + 1
-    if (i >= 2) and ((DP_MAXVAL[i-2]) + STEP[i] > max_val):
-        max_val = DP_MAXVAL[i-2] + STEP[i]
-        ONE_COUNT[i] = 1 # 주의: 첫번째 밟음
-    DP_MAXVAL[i] = max_val
-    print("i=", i, "ONE=", ONE_COUNT, "DP=", DP_MAXVAL)
-    
-print(DP_MAXVAL[count-1])
+print(max_str)
+print(min_str)
