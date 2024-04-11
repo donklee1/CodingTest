44f016977156d912b8dd756944db270e3f213c1e
동기 이 <ldk@true-tech.co.kr>
2024-04-12 00:18:25 +0900

    V0412H

--- Coding.py
+++ Coding.py
@@ -1,21 +1,30 @@
-# 1940 S4
+# 1012 S2
 from sys import stdin
 
-N = int(stdin.readline().rstrip())
-M = int(stdin.readline().rstrip())
-
-DATA = list(map(int, stdin.readline().split()))
-Used = [0] * N
+testcase = int(stdin.readline().rstrip())
 count = 0
-for i in range(N):
-    if (Used[i] == 1):
-        continue
-    for j in range(N):
-        if (Used[j] == 1) or (i == j):
-            continue
-        if (DATA[i] + DATA[j]) == M:
-            Used[i] = 1
-            Used[j] = 1
-            count += 1
 
-print(count)
+def DFS(x, y, rows, cols):
+    global count
+
+
+    for i in range(cols):
+        if (GROUND[y][x] == 1) and (Checked[y][x] == False):
+            Checked[y][x] = True
+            DFS(x+1, y, rows, cols) # --> 방향
+            DFS(x-1, y, rows, cols) # <-- 방향
+            DFS(x, y-1, rows, cols) # 위 방향
+            DFS(x, y+1, rows, cols) # 아래 방향
+
+
+for _ in range(testcase):
+    cols, rows, pos_count = map(int, stdin.readline().split())
+    GROUND = [[0] * cols for _ in range(rows)]
+    Checked = [[False] * cols for _ in range(rows)]
+    for i in range(pos_count):
+        x, y = map(int, stdin.readline().split())
+        GROUND[y][x] = 1
+
+    count = 0
+    DFS(0, 0, rows, cols)
+    print("count=", count)
