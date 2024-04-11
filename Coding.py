9f778814963ea2794b454685158ed2ca94d39971
donklee1 <ldk@true-tech.co.kr>
2024-04-11 20:47:03 +0900

    V0411

--- Coding.py
+++ Coding.py
@@ -1,36 +1,21 @@
-# 21314 S1
+# 1940 S4
 from sys import stdin
 
-S = stdin.readline().strip()
+N = int(stdin.readline().rstrip())
+M = int(stdin.readline().rstrip())
 
-# --- Token 분리 ---
-token = []
-sub_str = ""
-for s1 in S:
-    if s1 != "K":
-        sub_str += s1
-    else:
-        sub_str += "K"
-        token.append(sub_str)
-        sub_str = ""
-if (sub_str != ""):
-    token.append(sub_str)
+DATA = list(map(int, stdin.readline().split()))
+Used = [0] * N
+count = 0
+for i in range(N):
+    if (Used[i] == 1):
+        continue
+    for j in range(N):
+        if (Used[j] == 1) or (i == j):
+            continue
+        if (DATA[i] + DATA[j]) == M:
+            Used[i] = 1
+            Used[j] = 1
+            count += 1
 
-min_str = ""
-max_str = ""
-for tok in token:
-    m_count = str(tok).count("M")
-    print("tok=", tok)
-    if "K" in tok: # MMMMMK 타입
-        if m_count == 0:
-            max_str += "5"
-            min_str += "5"
-        else:
-            max_str += str(5 * (10 ** m_count)) # 500..
-            min_str += str((10 ** m_count) + 5) # 100.. 5
-    else: # MMMMM 타입
-        max_str += str(('1' * m_count))
-        min_str += str(10 ** (m_count-1))
-
-print(max_str)
-print(min_str)
+print(count)
