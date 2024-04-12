bb9ad710cfd185b15e942795354126a801c2e40f
donklee1 <ldk@true-tech.co.kr>
2024-04-12 18:31:49 +0900

    V0412

--- Coding.py
+++ Coding.py
@@ -1,30 +1,26 @@
-# 1012 S2
+# 21921 S3
 from sys import stdin
 
-testcase = int(stdin.readline().rstrip())
-count = 0
+N, X = map(int, stdin.readline().split())
+PEOPLES = list(map(int, stdin.readline().split()))
 
-def DFS(x, y, rows, cols):
-    global count
+SUBSUM = [0] * N
+SUBSUM[0] = PEOPLES[0]
+for i in range(1, X):
+    SUBSUM[i] = SUBSUM[i-1] + PEOPLES[i]
+same_count = 1
+max_sum = SUBSUM[i]
+for i in range(X, N):
+    SUBSUM[i] = SUBSUM[i-1] + PEOPLES[i]
+    part_sum = SUBSUM[i]-SUBSUM[i-X]
+    if part_sum > max_sum:
+        max_sum = part_sum
+        same_count = 1
+    elif part_sum == max_sum:
+        same_count += 1
 
-
-    for i in range(cols):
-        if (GROUND[y][x] == 1) and (Checked[y][x] == False):
-            Checked[y][x] = True
-            DFS(x+1, y, rows, cols) # --> 방향
-            DFS(x-1, y, rows, cols) # <-- 방향
-            DFS(x, y-1, rows, cols) # 위 방향
-            DFS(x, y+1, rows, cols) # 아래 방향
-
-
-for _ in range(testcase):
-    cols, rows, pos_count = map(int, stdin.readline().split())
-    GROUND = [[0] * cols for _ in range(rows)]
-    Checked = [[False] * cols for _ in range(rows)]
-    for i in range(pos_count):
-        x, y = map(int, stdin.readline().split())
-        GROUND[y][x] = 1
-
-    count = 0
-    DFS(0, 0, rows, cols)
-    print("count=", count)
+if max_sum == 0:
+    print("SAD")
+else:
+    print(max_sum)
+    print(same_count)
