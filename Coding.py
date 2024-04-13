ab2965604c7a1f147ae354865d8df1cf9764337c
동기 이 <ldk@true-tech.co.kr>
2024-04-13 12:47:53 +0900

    V0413H

--- Coding.py
+++ Coding.py
@@ -1,26 +1,52 @@
-# 21921 S3
+# 1406 S2 에디터
 from sys import stdin
 
-N, X = map(int, stdin.readline().split())
-PEOPLES = list(map(int, stdin.readline().split()))
+class ListNode:
+    def __init__(self, val=0, prev=None,next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
 
-SUBSUM = [0] * N
-SUBSUM[0] = PEOPLES[0]
-for i in range(1, X):
-    SUBSUM[i] = SUBSUM[i-1] + PEOPLES[i]
-same_count = 1
-max_sum = SUBSUM[i]
-for i in range(X, N):
-    SUBSUM[i] = SUBSUM[i-1] + PEOPLES[i]
-    part_sum = SUBSUM[i]-SUBSUM[i-X]
-    if part_sum > max_sum:
-        max_sum = part_sum
-        same_count = 1
-    elif part_sum == max_sum:
-        same_count += 1
+def PrintNode(node):
+    while node:
+        print(node.val, end="")
+        node = node.next
+    print("")
 
-if max_sum == 0:
-    print("SAD")
-else:
-    print(max_sum)
-    print(same_count)
+S = list(stdin.readline())
+command_count = int(stdin.readline().rstrip())
+
+head_node = ListNode(S[0])
+curr_node = head_node
+for i in range(1, len(S)):
+    new_node = ListNode(S[i])
+    curr_node.next = new_node
+    new_node.prev = curr_node
+    curr_node = curr_node.next
+
+PrintNode(head_node)
+
+for _ in range(command_count):
+    CMD_LIST = stdin.readline().split()
+    if CMD_LIST[0] == "L": # 커서 <-- 이동
+        if curr_node.prev != None:
+            curr_node = curr_node.prev
+    elif CMD_LIST[0] == "D": # 커서 --> 이동
+        if curr_node.next != None:
+            curr_node = curr_node.next
+    elif CMD_LIST[0] == "B": # 커서왼쪽 삭제
+        if curr_node.prev != None:
+            prev_node = curr_node.prev
+            next_node = curr_node.next
+            
+            #curr_node.prev = curr_node.prev.prev
+    elif CMD_LIST[0] == "P": # 삽입
+        new_node = ListNode(CMD_LIST[1], curr_node.prev, curr_node)
+        prev = new_node.prev
+        if prev != None:
+            prev.next = new_node
+        curr_node.prev = new_node
+
+print(head_node)
+PrintNode(head_node)
+
