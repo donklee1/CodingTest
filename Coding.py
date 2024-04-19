53726f19df81f2e2f62fd0a6c18fc06dcffce0b8
donklee1 <ldk@true-tech.co.kr>
2024-04-19 16:51:51 +0900

    V0419-2

--- Coding.py
+++ Coding.py
@@ -1,32 +1,18 @@
-# S1-1991 이진트리 탐색
+# S1-1629 곱셈
 from sys import stdin
-tree = dict() # 딕셔너리 사용!!
-count = int(input())
-for i in range(count):
-    root, left, right = input().split()
-    tree[root] = left,right # tuple과 동일 (left, right)
 
-def pre_order(data): # 성공
-    if data != ".":
-        print(data, end="")
-        pre_order(tree[data][0]) #튜플 첫항 액세스 방법
-        pre_order(tree[data][1]) #튜플 두번째 액세스 방법
+A, B, C = map(int, input().split())
 
-def in_order(data):
-    if data != ".":
-        left, right = tree[data]
-        in_order(left)
-        print(data, end="")
-        in_order(right)
+def power(A, B):
+    global C
+    if B == 1:
+        return A % C
+    
+    x = power(A, B // 2)
+    if (B % 2) == 0:
+        return (x * x) % C
+    else:
+        return (x * x) * A % C
 
-def post_order(data):
-    if data != ".":
-        post_order(tree[data][0])
-        post_order(tree[data][1])
-        print(data, end="")
-
-pre_order("A")
-print("")
-in_order("A")
-print("")
-post_order("A")
+result = power(A, B)
+print(result)
