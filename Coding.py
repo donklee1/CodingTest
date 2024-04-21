72a395f490147929627808a16542e835c6a262a6
동기 이 <ldk@true-tech.co.kr>
2024-04-21 23:57:30 +0900

    V0421H

--- Coding.py
+++ Coding.py
@@ -1,20 +1,9 @@
-# S2-1541 읽어버린 괄호
 from sys import stdin
 import re
 
-S = input()
-# L = re.split('([-|+|])', S)
-S2 = S.split("-")
-for idx, sub in enumerate(S2):
-    if "+" in sub:
-        S3 = sub.split("+")
-        val = 0
-        for c in S3:
-            val += int(c)
-        S2[idx] = val
-        
-result = int(S2[0])
-for i in range(1, len(S2)):
-    result -= S2[i]
-print("S2=", S2, result)
+S = 'apple orange:banana,tomato'
+L = re.split('[ ,:]', S)
+L2 = re.split('([ ,:])', S)
+print(L)
+print(L2)
 
--- S2/S2_1541!.py
+++ S2/S2_1541!.py
@@ -3,15 +3,10 @@ from sys import stdin
 import re
 
 S = input()
-# L = re.split('([-|+|])', S)
 S2 = S.split("-")
 for idx, sub in enumerate(S2):
     if "+" in sub:
-        S3 = sub.split("+")
-        val = 0
-        for c in S3:
-            val += int(c)
-        S2[idx] = val
+        S2[idx] = sum(map(int, sub.split("+")))
         
 result = int(S2[0])
 for i in range(1, len(S2)):
