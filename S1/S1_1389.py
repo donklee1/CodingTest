ab5a412b9a5516b0d1426c6df2cdb037071fbf62
donklee1 <ldk@true-tech.co.kr>
2024-04-23 18:17:12 +0900

    V0423-2

--- S1/S1_1389.py
+++ S1/S1_1389.py
@@ -1,11 +1,10 @@
-# S1_1389 케빈베이컨
+# S1_1389 케빈베이컨 (DFS, BFS 판단!!)
 from sys import stdin
 import sys
 from collections import deque
 
 user_count, relation_count = map(int, stdin.readline().split())
 GRAPH = [[] for _ in range(user_count+1)]
-RELATION = [[0] * (user_count+1) for _ in range(user_count+1)]
 for i in range(relation_count):
     man1, man2 = map(int, stdin.readline().split())
     GRAPH[man1].append(man2)
