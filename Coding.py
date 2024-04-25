353d188f4781d282311090b2f55642bcf47662a6
동기 이 <ldk@true-tech.co.kr>
2024-04-25 22:07:07 +0900

    V0425H2

--- Coding.py
+++ Coding.py
@@ -11,7 +11,7 @@ for i in range(N):
         org_x = L.index('2')
     MAP.append(L)
 
-RESULT = [[-1] * M for _ in range(N)]
+RESULT = [[-1] * M for _ in range(N)] # 도달못한 경우의 기본값 -1
 
 dx = [-1, 1,  0, 0]
 dy = [ 0, 0, -1, 1]
@@ -30,7 +30,7 @@ def BFS(y, x):
                 continue
             Visited[ny][nx] = 1
             if MAP[ny][nx] == '0':
-                RESULT[ny][nx] = 0 # 갈수없는땅
+                RESULT[ny][nx] = 0 # 갈수없는땅 --> 이 방법으로 완전하게 채울 수 없다 !!
                 continue
             if MAP[ny][nx] == '1': # 갈수있는길
                 RESULT[ny][nx] = dist
@@ -40,4 +40,7 @@ Visited = [[0] * M for _ in range(N)]
 BFS(org_y, org_x) # 주의 - 한번호출로 완전탐색 가능!!
 
 for i in range(N):
+    for j in range(M):
+        if MAP[i][j] == '0':
+            RESULT[i][j] = 0 # 벽위치 채움 
     print(*RESULT[i])
